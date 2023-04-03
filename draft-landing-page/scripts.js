a30740e9e317a5ce181dcaf43faeb71d3232157f
Laura Bizzle <laura.bizzle@open.partners>
2023-04-03 17:44:35 +0100

    one animated gif

---
+++ draft-landing-page/scripts.js
@@ -0,0 +1,9 @@
+const animatedImg = document.querySelector('.animated');
+
+animatedImg.addEventListener('mouseenter', () => {
+  animatedImg.src = 'https://the4thutility.co.uk/img/icons/animated/Save_White.gif';
+});
+
+animatedImg.addEventListener('mouseleave', () => {
+  animatedImg.src = 'https://the4thutility.co.uk/img/icons/animated/Save_White.gif';
+});
